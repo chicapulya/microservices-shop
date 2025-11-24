19b1f44dd4127add57221a7fc380539894833c3f
Ciorba Denis <sagas.spill-09@icloud.com>
2025-11-24 15:50:58 +0200

    написал приложение products, добавил товары в админку через фикстуры

--- services/product-service/config/urls.py
+++ services/product-service/config/urls.py
@@ -3,11 +3,10 @@ from django.urls import path, include
 from django.http import JsonResponse
 
 def health_check(request):
-    return JsonResponse({'status': 'healthy', 'service': 'user-service'})
+    return JsonResponse({'status': 'healthy', 'service': 'product-service'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', health_check),
-    path('api/auth/', include('apps.authentication.urls')),
-    path('api/users/', include('apps.users.urls')),
+    path('api/', include('apps.products.urls')),
 ]
